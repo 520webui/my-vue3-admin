8cc3b79850406bdd866a719ca46e78dd75bb7382
ex_qinji <qj@880615>
2023-02-03 18:27:02 +0800

    完善项目：引入了一些案例用vue3写

--- src/views/myVue/components/myCanvas/myCanvas.vue
+++ src/views/myVue/components/myCanvas/myCanvas.vue
@@ -1,53 +1,58 @@
 <template>
     <div class="myCanvas-wrap">
         <h3>图片的压缩处理</h3>
-        <canvas id="canvasImg" class="canvasImg"/>
+        <canvas ref="canvasImg" id="canvasImg" class="canvasImg"/>
     </div>
 </template>
 
-<script>
-    export default {
-        name: "myCanvas",
-        data () {
-            return {}
-        },
-        mounted () {
-            this.drawImg();
-            this.drawImgLine();
-        },
-        methods: {
-            /* 图片的处理压缩 画布*/
-            drawImg () {
-                // 创建画布
-                const canvas = document.getElementById('canvasImg');
-                const context = canvas.getContext('2d');
+<script lang="ts">
+    import {reactive, defineComponent, onMounted, ref, toRefs,computed,watch,defineProps } from 'vue';
+    import { useStore } from "vuex"
+    import { useRouter } from 'vue-router'
+    import cavasImg from "@/views/myVue/images/cavas-img.png"
+    export default defineComponent({
+      setup(props, contex) {
+        const dataMap = reactive({})
+        const canvasImg = ref(null)
+        const drawImg = () => {
+          // 创建画布
+          const canvas = document.getElementById('canvasImg');
+          const context = canvas.getContext('2d');
+          // 设置宽高度为等同于要压缩图片的尺寸
+          canvas.width = 400;
+          canvas.height = 300;
+          context.clearRect(0, 0, 400, 30);
+          let img = new Image();
+          img.src = cavasImg;
 
-                // 设置宽高度为等同于要压缩图片的尺寸
-                canvas.width = 400;
-                canvas.height = 300;
-                context.clearRect(0, 0, 400, 30);
-                let img = new Image();
-                img.src = require("../../images/cavas-img.png");
+          // 将img绘制到画布上
+          img.onload =  ()=> {
+            context.drawImage(img, 0, 0, 400, 300);
+          };
+        }
+        const drawImgLine = () => {
+          let canvas = document.getElementById("canvas");
+          if (canvas.getContext) {
+            let ctx = canvas.getContext("2d");
 
-                // 将img绘制到画布上
-                img.onload = function () {
-                    context.drawImage(img, 0, 0, 400, 300);
-                };
-            },
-            drawImgLine () {
-                let canvas = document.getElementById("canvas");
-                if (canvas.getContext) {
-                    let ctx = canvas.getContext("2d");
+            ctx.fillStyle = "rgb(200,0,0)";
+            ctx.fillRect(10, 10, 55, 50);
 
-                    ctx.fillStyle = "rgb(200,0,0)";
-                    ctx.fillRect(10, 10, 55, 50);
-
-                    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-                    ctx.fillRect(30, 30, 55, 50);
-                }
-            },
-        },
-    }
+            ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+            ctx.fillRect(30, 30, 55, 50);
+          }
+        }
+        onMounted(()=>{
+          drawImg()
+          // drawImgLine()
+        })
+        return{
+          ...toRefs(dataMap),
+          drawImg,
+          drawImgLine
+        }
+      }
+    })
 </script>
 
 <style lang="less" scoped>
--- src/views/myVue/myVue.vue
+++ src/views/myVue/myVue.vue
@@ -1,15 +1,21 @@
 <template>
     <div class="myVue-wrap">
         <div class="itme-box">
-            {{$t('mpanel.submit')}}
+          <the-scroll-box :loop="true"/>
         </div>
+        <div class="itme-box">
+          <my-canvas/>
+        </div>
+        <div class="itme-box">
+          <fill-rect-canvas/>
+        </div>
+        <div class="itme-box itme-box-relative"></div>
+        <div class="itme-box itme-box-relative"></div>
         <div class="itme-box">{{$t('menu.myVue')}}</div>
         <div class="itme-box">{{$t('table.tOperation')}}</div>
-        <div class="itme-box"></div>
-        <div class="itme-box"></div>
-        <div class="itme-box itme-box-relative"></div>
-        <div class="itme-box itme-box-relative"></div>
-        <div class="itme-box itme-box-relative"></div>
+        <div class="itme-box itme-box-relative">
+          {{$t('mpanel.submit')}}
+        </div>
         <div class="itme-box"></div>
         <div class="itme-box"></div>
         <div class="itme-box"></div>
@@ -25,7 +31,13 @@
     import {reactive, defineComponent, onMounted, ref, toRefs,computed,watch } from 'vue';
     import { useStore } from "vuex"
     import { useRouter } from 'vue-router'
+    import theScrollBox from "@/views/myVue/components/theScrollBox/scrollBox.vue"
+    import myCanvas from "@/views/myVue/components/myCanvas/myCanvas.vue"
+    import fillRectCanvas from "@/views/myVue/components/myCanvas/fillRectCanvas.vue"
     export default defineComponent({
+      components: {
+        theScrollBox,myCanvas,fillRectCanvas
+      },
         setup() {
             const store = useStore();
             const router = useRouter();
